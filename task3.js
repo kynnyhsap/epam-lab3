dd6e1632f9f588554047b9e962a8e224774bf8bf
Andrew Pashinnik <tobirawork@gmail.com>
2020-03-17 23:07:43 +0200

    feat: task3

---
+++ task3.js
@@ -0,0 +1,21 @@
+const DEFAULT_BOTTLES = 50;
+
+function showMessage(bottlesLeft) {
+    if (bottlesLeft < 0) {
+        return
+    }
+
+    if (bottlesLeft == 0) {
+        console.log('жодної!')
+        return
+    }
+
+    console.log(`${bottlesLeft} стоїть на стіні, одна упала і залишилось`)
+}
+
+
+
+for (let i = DEFAULT_BOTTLES; i >= 0; i--) {
+    showMessage(i)
+}
+
