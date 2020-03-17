87a80726fd43a8b02cfefade371b96046a5b65f2
Andrew Pashinnik <tobirawork@gmail.com>
2020-03-17 23:41:08 +0200

    feat: task2

---
+++ task2.js
@@ -0,0 +1,35 @@
+function printRow(row, padding) {
+    console.log(' '.repeat(padding) + row.join(' '))
+}
+
+function drawPascalTriangle(n) {
+    if (n < 1) {
+        return;
+    }
+
+    let prev = [1];
+    printRow(prev, n)
+
+    if (n === 1) {
+        return;
+    }
+
+    for (let i = 2; i <= n; i++) {
+        const current = Array(i).fill(0);
+
+        for (let j = 0; j < i; j++) {
+            if (j === 0 || j === i - 1) {
+                current[j] = 1;
+                continue;
+            }
+
+            current[j] = prev[j - 1] + prev[j]
+        }
+
+        printRow(current, n - i)
+
+        prev = current
+    }
+}
+
+drawPascalTriangle(6)
